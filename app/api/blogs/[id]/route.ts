6aae4bc4d91b1a32a2c79f3d644e252812c853ee
CodeSandbox <support@codesandbox.io>
2023-05-21 13:05:49 +0000

    Initial commit

---
+++ app/api/blogs/[id]/route.ts
@@ -0,0 +1,14 @@
+export const GET = async (req: Request, res:Response) => {
+  console.log("GET ONE REQUEST");
+  // Get a post blog by id
+};
+
+export const PUT = async (req: Request, res:Response) => {
+  console.log("Update REQUEST");
+  // Update a post blog by id
+};
+
+export const DELETE = async (req: Request, res:Response) => {
+  console.log("Delete REQUEST");
+  // Delete a post blog by id
+};
---
+++ lib/data.ts
@@ -0,0 +1,36 @@
+import { type } from "os";
+
+type Post = {
+  id: string;
+  title: string;
+  desc: string;
+  date: Date;
+};
+
+let posts: Post[] = [];
+
+// handlers functions
+export const getPosts = () => posts;
+
+export const addPost = (post: Post) => {
+  posts.push(post);
+};
+
+export const deletePost = (id: string) => {
+  const posts = posts.filter((post) => post.id !== id);
+};
+
+export const updatePost = (id: string, title: string, desc: string) => {
+  const post = posts.find((post) => post.id === id);
+
+  if (post) {
+    post.title = title;
+    post.desc = desc;
+  } else {
+    throw new Error("No posts found");
+  }
+};
+
+export const getPostById = (id: string) => {
+  return posts.find((post) => post.id === id);
+};
